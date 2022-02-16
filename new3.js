a5423e8b004de45e181150ee467d2c24d8ea5440
Jesse <lkmwyeb@gmail.com>
2022-02-16 17:01:25 +0800

    Add files via upload

---
+++ new3.js
@@ -0,0 +1,55 @@
+AKIDPbZJo5wHxXuBF3PHQO8fwPbebKhrgRQA
+
+oa6ey894kDbrpc9Yv6LCBdrsiT4SbfZ7
+
+avatar-1304137467
+
+message-1304137467
+
+for(var i=1;i<=1000000;i++){
+db.msg.insert({
+     _id:NumberLong(new Date().getTime()*1000),
+		 clientMessageId:"6acd69f5b3c04f1d8f282d33a6be8715",
+		 senderId:NumberInt("10024002"),
+		 targetId:NumberInt("10000084"),
+		 type:NumberInt("2"),
+		 messageTypeu:NumberInt("0"),
+		 messageTemp:"MC:TxtMsg",
+		 fp:"e95f5336d2fa4e5ea17f6cfe54386170",
+		 content:"{\"i\":"+ NumberLong(new Date().getTime()*1000) +",\"ty\":2,\"d\":\"{\\\"content\\\":\\\""+ NumberLong(new Date().getTime()) +"\\\",\\\"cMsgId\\\":\\\"a7c78282f55b8425189b6b085352fd070\\\"}\",\"f\":\"10024002\",\"t\":\"10000084\",\"fp\":\"dcd74f5c6e064b2587914d7807e45737\",\"mt\":\"0\",\"m\":\"MC:TxtMsg\",\"ts\":1606358432597,\"v\":1,\"q\":true}",
+		 qos:true,
+		 text:"B_"+NumberLong(new Date().getTime()),
+		 sendTime:NumberLong(new Date().getTime()),
+		 version:NumberInt("1")
+ })
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+私钥 4225162211
+私钥:
+0330a0d2de42ed23b6d43dc8b649688acdd43b8403344e931523688692648b66
+公钥：
+8aaa17d8984d62e7ca1700b18090893a8b53e693c18192ea2e6ab1e1342cfbeab248f89a97be8f0c442e3944fe8540e2
+种子：
+shuffle air parent rival chase worth uphold elbow raccoon ask wheel patch filter shoe thought like liar cactus time train emerge orchard unfold disagree
